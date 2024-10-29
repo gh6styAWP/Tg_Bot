80dfaa3658d1850f58bb6bd0a18e0ce51fa264fe
Ruslan Gafarov <gafarov-ruslan@internet.ru>
2024-10-29 22:54:20 +0300

    Реализована функция извлечения токена из файла

--- Tg_Bot/Tg_Bot.cpp
+++ Tg_Bot/Tg_Bot.cpp
@@ -1,9 +1,33 @@
 ﻿#include <iostream>
 #include <tgbot/tgbot.h>
+#include <fstream>
 using namespace std; 
+
+//функция извлечения токена из файла
+string getToken(const string& filePath) {
+    ifstream file(filePath);
+
+    if (!file.is_open())
+        throw runtime_error("Не удалось открыть файл с токеном.");
+
+    string token;
+    getline(file, token);
+    return token;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Ru");
+
+    string filePath = "C:\\Users\\gafar\\Desktop\\token.txt";
+    string token;
+    try {
+        token = getToken(filePath);
+    }
+    catch(const exception& e) {
+        cerr << "Ошибка: " << e.what() << endl;
+        return 1;
+    }
+
 }
 
